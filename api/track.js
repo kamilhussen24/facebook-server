3909d664de878d8abf61a3e80934b4bef0e0c07e
kamilhussen24 <76224565+kamilhussen24@users.noreply.github.com>
2025-06-29 21:43:28 +0600

    update from dex

--- api/track.js
+++ api/track.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const pixel_id = '1211731600730925';
-  const access_token = 'EAARvr019SPQBO2Qe2JtkRZBe8SXEsHf0lsKexJhMsxYZBXK0kefGE2wyuyUhAOTqqGSprpt2y4v1aMzB1ZAiOqcZBnSwYcoktNbgy6lYI7hGw4ZAqe2ZByPRwr21McxLskrFBOYSZAZCfZBdTirWHPkjjeEbO8BHrMWsTgZAgTdJGGTVgIrcQfKuoVf38xNdkQXwZDZD'; // shorten for security
+  const access_token = 'EAARvr019SPQBO2Qe2JtkRZBe8SXEsHf0lsKexJhMsxYZBXK0kefGE2wyuyUhAOTqqGSprpt2y4v1aMzB1ZAiOqcZBnSwYcoktNbgy6lYI7hGw4ZAqe2ZByPRwr21McxLskrFBOYSZAZCfZBdTirWHPkjjeEbO8BHrMWsTgZAgTdJGGTVgIrcQfKuoVf38xNdkQXwZDZD'; // For security, keep this in .env in production
 
   const { event_name, event_source_url } = req.body;
 
@@ -30,6 +30,7 @@ export default async function handler(req, res) {
         event_time: Math.floor(Date.now() / 1000),
         action_source: 'website',
         event_source_url,
+        test_event_code: 'TEST12048', // Test Events Manager 
         user_data: {
           em: hashSHA256(userEmail),
           client_user_agent: req.headers['user-agent'],
